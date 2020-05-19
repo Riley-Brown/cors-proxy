6260edf91074df98857eba704cb63c24b17fcfea
Riley Brown <riley@riley.gg>
2020-05-19 12:29:11 -0400

    delete unneeded get url route path

--- src/http/get-url/index.js
+++
@@ -1,7 +0,0 @@
-// learn more about HTTP functions here: https://arc.codes/primitives/http
-exports.handler = async function http(req) {
-  return {
-    headers: { "content-type": "text/html; charset=utf8" },
-    body: "hey"
-  };
-};
